f9e9f37374747f6efa28c2696b685013f8111187
actionless <actionless.loveless@gmail.com>
2013-11-14 20:11:19 +0300

    added profile test (3.10)

---
+++ tests/test_profile.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+
+from test_tool.helpers.selenium_stuff import navigate
+from test_tool.helpers.actions.auth import log_in_as_user_if_necessary
+from test_tool.api.sections.auth import profile as api_profile
+
+from tests import test_data
+
+
+class ProfileTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        #if 'foobar' not in SERVER_URL:
+        #    raise SkipTest("new advertisement functionality enabled only on 'devel' branch")
+        cls.session = test_data.session
+        cls.browser = test_data.browser_embed
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_profile(self):
+        """
+        This test verifies if user can login and if user dashboard gives correct info.
+        """
+        self.browser.get(navigate('/dashboard'))
+        log_in_as_user_if_necessary(self.browser)
+        frontend_first_name = self.browser.find_element_by_id('first_name').get_attribute('value')
+        api_first_name = api_profile(self.session)['first_name']
+        self.assertEqual(api_first_name, frontend_first_name,
+                         'first_name not matches in frontend and API')
