6751f2ff89f81596e3a44538d2c0be096239f8c1
actionless <actionless.loveless@gmail.com>
2013-11-19 18:49:46 +0300

    added xUnit support

--- headless.sh
+++ headless.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DISPLAY=:2 xvfb-run --auto-servernum nosetests -sv --logging-level=ERROR
+DISPLAY=:2 xvfb-run --auto-servernum nosetests -sv --logging-level=ERROR --with-xunit
 
 #or
 #Xvfb :2 -screen 0 1920x1080x24 &
