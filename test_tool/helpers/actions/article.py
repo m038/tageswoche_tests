a377ca444537efc41013c9700c500d7c32dcc040
actionless <actionless.loveless@gmail.com>
2013-11-20 19:00:06 +0300

    generalized article stuff

---
+++ test_tool/helpers/actions/article.py
@@ -0,0 +1,56 @@
+from selenium.webdriver.support.ui import Select, WebDriverWait
+
+from test_tool.settings import LONG_AJAX
+from test_tool.helpers.selenium_stuff import navigate, id_generator, wait_for_visible
+from test_tool.logger import logger
+
+
+def create_new_article(browser, article_title, article_type, article_language, article_publication, article_section):
+    browser.get(navigate('/admin/articles/add_move.php'))
+
+    browser.find_element_by_css_selector('input[name="f_article_name"]').send_keys(article_title)
+    Select(browser.find_element_by_css_selector('select[name="f_article_type"]'))\
+        .select_by_visible_text(article_type)
+    Select(browser.find_element_by_css_selector('select[name="f_article_language"]'))\
+        .select_by_visible_text(article_language)
+    Select(browser.find_element_by_css_selector('select[name="f_destination_publication_id"]'))\
+        .select_by_visible_text(article_publication)
+
+    destination_issue_select = Select(
+        wait_for_visible(browser, LONG_AJAX,
+            lambda br: br.find_element_by_css_selector('select[name="f_destination_issue_number"]')
+        )
+    )
+    most_recent_article = str(max(
+        [int(o.get_attribute('value')) for o in destination_issue_select.options]
+    ))
+    destination_issue_select.select_by_value(most_recent_article)
+
+    Select(wait_for_visible(browser, LONG_AJAX,
+            lambda br: br.find_element_by_css_selector('select[name="f_destination_section_number"]')))\
+        .select_by_visible_text(article_section)
+
+    browser.find_element_by_css_selector('input[name="save"]').click()
+
+
+def publish_article(browser, article_content):
+    mce_frame = WebDriverWait(browser, LONG_AJAX).until(
+            lambda br: br.find_element_by_css_selector('.tinyMCEHolder iframe')
+    ) # the first mce iframe
+    browser.switch_to_frame(mce_frame)
+    browser.find_element_by_css_selector('body#tinymce').send_keys(article_content)
+    browser.switch_to_default_content()
+
+    browser.find_element_by_css_selector('input[id="save"]').click()
+    article_saved_popup = lambda br:\
+        True if max(
+            [element.text == 'Article saved.' for element
+            in br.find_elements_by_css_selector('div.flash')]
+        ) else None
+    WebDriverWait(browser, LONG_AJAX).until(article_saved_popup)
+    logger.debug('popup')
+
+    Select(browser.find_element_by_css_selector('select[name="f_action_workflow"]'))\
+        .select_by_value('Y')
+    webcode = browser.find_element_by_xpath('/html/body/div[4]/div[3]/div[5]/div[2]/dl/dd[5]').text
+    return webcode
--- tests/admin/test_article.py
+++ tests/admin/test_article.py
@@ -1,10 +1,7 @@
 from unittest import TestCase
-from selenium.webdriver.support.ui import Select, WebDriverWait
 
-from test_tool.settings import LONG_AJAX
-from test_tool.helpers.selenium_stuff import navigate, id_generator, wait_for_visible
-from test_tool.helpers.actions.auth import log_in_as_user_if_necessary
-from test_tool.logger import logger
+from test_tool.helpers.selenium_stuff import navigate, id_generator
+from test_tool.helpers.actions.article import create_new_article, publish_article
 
 from tests import test_data
 
@@ -36,60 +33,15 @@ class ProfileTestCase(TestCase):
         """
         article_title = id_generator()
         article_content = ' '.join([id_generator() for i in range(20)]) # 20 random "words"
-
-        #first page
-        #{{{
-        self.browser_admin.get(navigate('/admin/articles/add_move.php'))
-
-        self.browser_admin.find_element_by_css_selector('input[name="f_article_name"]').send_keys(article_title)
-        Select(self.browser_admin.find_element_by_css_selector('select[name="f_article_type"]'))\
-            .select_by_visible_text('news')
-        Select(self.browser_admin.find_element_by_css_selector('select[name="f_article_language"]'))\
-            .select_by_visible_text('Deutsch')
-        Select(self.browser_admin.find_element_by_css_selector('select[name="f_destination_publication_id"]'))\
-            .select_by_visible_text('Tageswoche')
-
-        destination_issue_select = Select(
-            wait_for_visible(self.browser_admin, LONG_AJAX,
-                lambda br: br.find_element_by_css_selector('select[name="f_destination_issue_number"]')
-            )
+        create_new_article(
+            browser=self.browser_admin,
+            article_title=article_title,
+            article_type = 'news',
+            article_language = 'Deutsch',
+            article_publication = 'Tageswoche',
+            article_section = 'Basel'
         )
-        most_recent_article = str(max(
-            [int(o.get_attribute('value')) for o in destination_issue_select.options]
-        ))
-        destination_issue_select.select_by_value(most_recent_article)
-
-        Select(wait_for_visible(self.browser_admin, LONG_AJAX,
-                lambda br: br.find_element_by_css_selector('select[name="f_destination_section_number"]')))\
-            .select_by_visible_text('Basel')
-
-        self.browser_admin.find_element_by_css_selector('input[name="save"]').click()
-        #}}}
-
-        #second page
-        #{{{
-        mce_frame = WebDriverWait(self.browser_admin, LONG_AJAX).until(
-                lambda br: br.find_element_by_css_selector('.tinyMCEHolder iframe')
-        ) # the first mce iframe
-        self.browser_admin.switch_to_frame(mce_frame)
-        self.browser_admin.find_element_by_css_selector('body#tinymce').send_keys(article_content)
-        self.browser_admin.switch_to_default_content()
-
-        self.browser_admin.find_element_by_css_selector('input[id="save"]').click()
-        article_saved_popup = lambda br:\
-            True if max(
-                [element.text == 'Article saved.' for element
-                in br.find_elements_by_css_selector('div.flash')]
-            ) else None
-        WebDriverWait(self.browser_admin, LONG_AJAX).until(article_saved_popup)
-        logger.debug('popup')
-
-        Select(self.browser_admin.find_element_by_css_selector('select[name="f_action_workflow"]'))\
-            .select_by_value('Y')
-        webcode = self.browser_admin.find_element_by_xpath('/html/body/div[4]/div[3]/div[5]/div[2]/dl/dd[5]').text
-        #}}}
-
-        # checking on frontend
+        webcode = publish_article(self.browser_admin, article_content)
         self.browser.get(navigate(webcode))
         result_title = self.browser.find_element_by_css_selector('article h2').text
         self.assertEqual(article_title, result_title,
