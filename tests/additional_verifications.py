650e0a8221eff131c787b0e9aa2042dff08b7743
actionless <actionless.loveless@gmail.com>
2013-11-27 14:05:06 +0300

    added visibility check for test class addin

--- tests/additional_verifications.py
+++ tests/additional_verifications.py
@@ -7,39 +7,50 @@ from test_tool.settings import MAX_WAIT
 class AdditionalVerifiesTestClass(object):
 
     def verify_selector(self, selector):
+        def verify_function(br):
+            result = br.find_element_by_css_selector(selector)
+            if result.is_displayed():
+                return result
+            else:
+                return None
         try:
-            return WebDriverWait(self.browser, MAX_WAIT).until(
-                lambda br: br.find_element_by_css_selector(selector)
-            )
+            return WebDriverWait(self.browser, MAX_WAIT).until(verify_function)
         except TimeoutException:
-            self.fail("No '{selector}'".format(selector=selector))
+            self.fail("No '{selector}' on page.".format(selector=selector))
 
     def verify_text_in_selector(self, selector, text):
+        def verify_function(br):
+            result = br.find_element_by_css_selector(selector)
+            if result.is_displayed() and text in result.text:
+                return result
+            else:
+                return None
         try:
-            WebDriverWait(self.browser, MAX_WAIT).until(
-                lambda br: True if text in
-                br.find_element_by_css_selector(selector).text
-                else None
-            )
+            return WebDriverWait(self.browser, MAX_WAIT).until(verify_function)
         except TimeoutException:
-            print(self.browser.find_element_by_css_selector(selector).text)
             self.fail(u"'{text}' is not in '{selector}'".format(text=text, selector=selector))
 
     def verify_text_in_selectors_attribute(self, selector, attribute, text):
+        def verify_function(br):
+            result = br.find_element_by_css_selector(selector)
+            if result.is_displayed() and text in result.get_attribute(attribute):
+                return result
+            else:
+                return None
         try:
-            WebDriverWait(self.browser, MAX_WAIT).until(
-                lambda br: True if text in
-                br.find_element_by_css_selector(selector).get_attribute(attribute)
-                else None
-            )
+            WebDriverWait(self.browser, MAX_WAIT).until(verify_function)
         except TimeoutException:
             self.fail(u"{text} is not in '{selector}'".format(text=text, selector=selector))
 
     def verify_number_of_elements(self, selector, number):
+        def verify_function(br):
+            results = br.find_elements_by_css_selector(selector)
+            visible_results = [result for result in results if result.is_displayed()]
+            if len(visible_results) == number:
+                return visible_results
+            else:
+                return None
         try:
-            WebDriverWait(self.browser, MAX_WAIT).until(
-                lambda br: True if len(br.find_elements_by_css_selector(selector)) == number
-                else None
-            )
+            WebDriverWait(self.browser, MAX_WAIT).until(verify_function)
         except TimeoutException:
             self.fail("No {number} of '{selector}'".format(number=number, selector=selector))
