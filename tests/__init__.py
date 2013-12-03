a5a1865d18613c398b7c128cf6486f939f619f38
actionless <actionless.loveless@gmail.com>
2013-12-03 17:45:46 +0300

    optimized init for different server types

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,8 @@
 from requests import Session
 import threading
 
-from test_tool.settings import ADMIN_LOGIN, ADMIN_PASS, BLOGGER_LOGIN, BLOGGER_PASS, USER_MAIL, USER_PASS
+from test_tool.settings import (ADMIN_LOGIN, ADMIN_PASS, BLOGGER_LOGIN, BLOGGER_PASS, USER_MAIL, USER_PASS,
+                                PRODUCTION)
 from test_tool.helpers.actions.auth import log_in_to_admin_backend_if_necessary, log_in_if_necessary
 from test_tool.helpers.selenium_stuff import new_webdriver, navigate
 
@@ -12,14 +13,8 @@ test_data = threading.local()
 def setUpModule():
     global test_data
     test_data.session = Session()
-    test_data.browser_admin = new_webdriver()
     test_data.browser_guest = new_webdriver()
     test_data.browser_user = new_webdriver()
-    test_data.browser_blogger = new_webdriver()
-
-    browser_admin = test_data.browser_admin
-    browser_blogger = test_data.browser_blogger
-    browser_user = test_data.browser_user
 
     def log_in_user():
         browser_user.get(navigate('/auth'))
@@ -33,19 +28,28 @@ def setUpModule():
         browser_blogger.get(navigate('/admin'))
         log_in_to_admin_backend_if_necessary(browser_blogger, username=BLOGGER_LOGIN, password=BLOGGER_PASS)
 
+    browser_user = test_data.browser_user
     thread1 = threading.Thread(target=log_in_user)
     thread1.start()
-    thread2 = threading.Thread(target=log_in_admin)
-    thread2.start()
-    thread3 = threading.Thread(target=log_in_blogger)
-    thread3.start()
+
+    if not PRODUCTION:
+        test_data.browser_admin = new_webdriver()
+        test_data.browser_blogger = new_webdriver()
+        browser_admin = test_data.browser_admin
+        browser_blogger = test_data.browser_blogger
+        thread2 = threading.Thread(target=log_in_admin)
+        thread2.start()
+        thread3 = threading.Thread(target=log_in_blogger)
+        thread3.start()
+
     while threading.active_count() > 1:
         pass
 
 
 def tearDownModule():
     global test_data
-    test_data.browser_admin.close()
     test_data.browser_guest.close()
     test_data.browser_user.close()
-    test_data.browser_blogger.close()
+    if not PRODUCTION:
+        test_data.browser_admin.close()
+        test_data.browser_blogger.close()
