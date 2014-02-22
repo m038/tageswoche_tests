97c5cac25dea9280330f22975684aee04dcbf879
actionless <actionless.loveless@gmail.com>
2014-02-22 14:26:30 +0300

    updated markup for comments' helpers

--- test_tool/helpers/actions/admin/comments.py
+++ test_tool/helpers/actions/admin/comments.py
@@ -1,24 +1,20 @@
-from selenium.webdriver.support.ui import Select
-
 from test_tool.settings import DEFAULT_WAIT, LONG_AJAX
 from test_tool.helpers.selenium_stuff import(
-    wait_for_visible, scroll_to, hover)
+    scroll_to, hover, wait_for_visible_by_css)
 
 
 def get_list_of_comments(browser):
-    wait_for_visible(
-        browser, LONG_AJAX,
-        lambda br: br.find_element_by_css_selector('tbody tbody tr'))
+    wait_for_visible_by_css(browser, LONG_AJAX, 'tbody tr#use-hover')
     comments_elements = browser.find_elements_by_css_selector(
-        'tbody tr.status_approved')
+        'tbody tr#use-hover')
     comments = []
     for element in comments_elements:
         comments.append({
             'element': element,
             'subject': element.find_element_by_css_selector(
-                'td.commentMessage span.commentSubject').text,
+                'span.commentSubject').text,
             'content': element.find_element_by_css_selector(
-                'td.commentMessage p.commentBody').text,
+                'p.commentBody').text,
         })
     return comments
 
@@ -35,22 +31,19 @@ def find_comment_element_on_backend(browser, comment_text):
 def recommend_comment(browser, comment_element):
     scroll_to(comment_element)
     hover(browser, comment_element)
-    recommend_button = wait_for_visible(
-        comment_element, DEFAULT_WAIT,
-        lambda el: el.find_element_by_css_selector('a.action-recommend'))
+    recommend_button = wait_for_visible_by_css(
+        browser, DEFAULT_WAIT, 'a.action-recommend')
     recommend_button.click()
-    wait_for_visible(browser, LONG_AJAX,
-                     lambda br: br.find_element_by_css_selector('div.flash'))
+    wait_for_visible_by_css(browser, LONG_AJAX, 'div.flash')
 
 
 def make_comment_good(browser, comment_element):
     scroll_to(comment_element)
     hover(browser, comment_element)
-    good_button = wait_for_visible(
+    list_select = wait_for_visible_by_css(
+        browser, DEFAULT_WAIT, 'ul.select2-choices')
+    list_select.click()
+    good_choice = wait_for_visible_by_css(
         comment_element, DEFAULT_WAIT,
-        lambda el: el.find_element_by_xpath('//td[1]/div/ul/li[8]/a'))
-    good_button.click()
-    Select(
-        browser.find_element_by_css_selector(
-            'select[name="f_action_workflow"]')
-    ).select_by_value('Y')
+        'ul.select2-results div.select2-result-label')
+    good_choice.click()
