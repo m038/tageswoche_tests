5262c3ed47ff02e9bf4b7cd3fc3b2a9c67567d76
actionless <actionless.loveless@gmail.com>
2014-02-22 14:52:12 +0300

    added additional verifications for ubersicht test

--- tests/frontend/test_themen_topic_with_dossier.py
+++ tests/frontend/test_themen_topic_with_dossier.py
@@ -14,7 +14,9 @@ class ThemenPageWithDossierTestCase(TestCase, AdditionalVerifiesTestClass):
         print('*Topic/Themen Topic page without dossier')
         cls.session = test_data.session
         cls.browser = test_data.browser_guest
-        cls.api_result = api_get(uri='/api/dossiers/articles?dossier_id=745&client=ipad&version=1', session=cls.session)
+        cls.api_result = api_get(
+            uri='/api/dossiers/articles?dossier_id=745&client=ipad&version=1',
+            session=cls.session)
         cls.browser.get(navigate('/themen/Messe%20Basel'))
 
     @classmethod
@@ -37,7 +39,9 @@ class ThemenPageWithDossierTestCase(TestCase, AdditionalVerifiesTestClass):
                                   self.browser.find_elements_by_css_selector(
                                       '.dossier-section article h2 a'
                                   )]
+        self.assertEqual(check_articles, len(article_links_frontend))
         article_ids_api = [record['article_id'] for record in self.api_result]
+        self.assertEqual(check_articles, len(article_ids_api))
         for i in range(check_articles):
             if str(article_ids_api[i]) not in article_links_frontend[i]:
                 self.fail("{0}-st article id not matches ({1}, {2}).".format(
